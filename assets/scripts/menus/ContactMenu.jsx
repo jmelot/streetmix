a3ba613f96f2518585266194665e1c1d5a1b89ef
Jennifer Melot <jtmelot@gmail.com>
2019-02-03 17:06:05 -0500

    ContactMenu now uses MenuListItem components

--- assets/scripts/menus/ContactMenu.jsx
+++ assets/scripts/menus/ContactMenu.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { FormattedMessage } from 'react-intl'
 import Menu from './Menu'
-import Icon from '../ui/Icon'
 import { trackEvent } from '../app/event_tracking'
 import { showDialog } from '../store/actions/dialogs'
+import MenuListItem from './MenuListItem'
 
 class ContactMenu extends React.PureComponent {
   static propTypes = {
@@ -19,25 +18,18 @@ class ContactMenu extends React.PureComponent {
   render () {
     return (
       <Menu {...this.props}>
-        <a href="https://discord.gg/NsKmV3S" target="_blank" rel="noopener noreferrer">
-          <Icon icon="discord" />
-          <FormattedMessage id="menu.contact.discord" defaultMessage="Join Discord chat" />
-        </a>
-        <a href="https://forums.streetmix.net/" target="_blank" rel="noopener noreferrer">
-          <Icon icon="forums" />
-          <FormattedMessage id="menu.contact.forums" defaultMessage="Discuss on the forums" />
-        </a>
-        <a href="https://github.com/streetmix/streetmix/" target="_blank" rel="noopener noreferrer">
-          <Icon icon="github" />
-          <FormattedMessage id="menu.contact.github" defaultMessage="View source code on GitHub" />
-        </a>
-        <a href="https://twitter.com/intent/tweet?text=@streetmix" target="_blank" rel="noopener noreferrer">
-          <Icon icon="twitter" />
-          <FormattedMessage id="menu.contact.twitter" defaultMessage="Send a tweet to @streetmix" />
-        </a>
-        <a href="#" onClick={this.props.showMinecraftDialog}>
-          <FormattedMessage id="menu.contact.minecraft" defaultMessage="Play Minecraft with us!&lrm;" />
-        </a>
+        <ul className="menu-item-group">
+          <MenuListItem href="https://discord.gg/NsKmV3S" icon="discord" messageId="menu.contact.discord"
+            defaultMessage="Join Discord chat" />
+          <MenuListItem href="https://forums.streetmix.net/" icon="forums" messageId="menu.contact.forums"
+            defaultMessage="Discuss on the forums" />
+          <MenuListItem href="https://github.com/streetmix/streetmix/" icon="github" messageId="menu.contact.github"
+            defaultMessage="View source code on GitHub" />
+          <MenuListItem href="https://twitter.com/intent/tweet?text=@streetmix" icon="twitter"
+            messageId="menu.contact.twitter" defaultMessage="Send a tweet to @streetmix" />
+          <MenuListItem href="#" messageId="menu.contact.minecraft" defaultMessage="Play Minecraft with us!&lrm;"
+            clickHandler={this.props.showMinecraftDialog} />
+        </ul>
       </Menu>
     )
   }
---
+++ assets/scripts/menus/MenuListItem.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Icon from '../ui/Icon'
+import { FormattedMessage } from 'react-intl'
+
+export default class MenuListItem extends React.PureComponent {
+  static propTypes = {
+    href: PropTypes.string,
+    clickHandler: PropTypes.func,
+    icon: PropTypes.string,
+    messageId: PropTypes.string,
+    defaultMessage: PropTypes.string,
+    children: PropTypes.node
+  }
+
+  render () {
+    const linkParams = {
+      'href': this.props.href
+    }
+    if (this.props.clickHandler === undefined) {
+      linkParams['target'] = '_blank'
+      linkParams['rel'] = 'noopener noreferrer'
+    } else {
+      linkParams['onClick'] = this.props.clickHandler
+    }
+    return (
+      <li>
+        <a {...linkParams}>
+          {this.props.icon !== undefined &&
+            <Icon icon={this.props.icon} />
+          }
+          <FormattedMessage id={this.props.messageId} defaultMessage={this.props.defaultMessage} />
+        </a>
+        {this.props.children}
+      </li>
+    )
+  }
+}
